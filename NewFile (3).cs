bdeb946e43b4ce80896d94021ad594edd621d53e
Vlad <guselnikovvi@gmail.com>
2015-06-02 14:49:29 +0600

    Generate OverWrite(), add TestOverWrite

---
+++ NewFile (3).cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace tdd
+{
+    public class NewFile
+    {
+        int sl_count;
+        public string way, sl;
+        public NewFile()
+        {
+            way = @"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt";
+
+        }
+        public void SetWay(string p)
+        {
+            way = p;
+        }
+
+        public void MakeNew()
+        {
+            FileStream fs = File.Create(way);
+            fs.Close();
+
+            StreamWriter sw = new StreamWriter(way);
+            sw.WriteLine("0000000000");
+            sw.Close();
+        }
+
+        public void OverWrite()
+        {
+            int i;
+
+
+            StreamReader sr = new StreamReader(way);
+            sl = sr.ReadToEnd();
+            sr.Close();
+            sl_count = sl.Length - 2;
+            StreamWriter sw = new StreamWriter(way);
+            // sw.Dispose();
+
+            for (i = 0; i < sl_count; i++)
+                sw.Write("1");
+            sw.Close();
+            sw = new StreamWriter(way);
+            for (i = 0; i < sl_count; i++)
+                sw.Write("2");
+            sw.Close();
+        }
+    }
+}
---
+++ UnitTest1 (3).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tdd;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSetWay()
+        {
+            var test_file = new NewFile();
+
+            test_file.SetWay(@"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+            string result = test_file.way;
+            Assert.IsTrue(result==@"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+
+        }
+        [TestMethod]
+        public void TestMakeNew()
+        {
+            var test_file = new NewFile();
+            test_file.MakeNew();
+            var result = test_file.way;
+            Assert.IsTrue(result == @"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+
+
+        }
+
+        [TestMethod]
+        public void TestOverWrite()
+        {
+            var test_file = new NewFile();
+            test_file.OverWrite();
+            var result = test_file.sl;
+            Assert.IsTrue(result == "0000000000\r\n");
+
+
+        }
+        
+       
+    }
+}
