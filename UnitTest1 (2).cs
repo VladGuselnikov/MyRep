ae49ac74820da893d0ce6527d2af4a7bfe5e0aff
Vlad <guselnikovvi@gmail.com>
2015-06-02 14:38:22 +0600

    Add constructor in NewFile, add TestMakeNew()

---
+++ UnitTest1 (2).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tdd;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSetWay()
+        {
+            var test_file = new NewFile();
+
+            test_file.SetWay(@"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+            string result = test_file.way;
+            Assert.IsTrue(result==@"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+
+        }
+        [TestMethod]
+        public void TestMakeNew()
+        {
+            var test_file = new NewFile();
+            test_file.MakeNew();
+            var result = test_file.way;
+            Assert.IsTrue(result == @"D:\ПОЛИТЕХ\ПИ12А-8сем\vertify15\лабы\tdd\tdd\bin\Debug\myfile.txt");
+
+
+        }
+        
+       
+    }
+}
