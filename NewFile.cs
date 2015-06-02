0ea2b12965bed78dca7319ce2f49b436c421a218
Vlad <guselnikovvi@gmail.com>
2015-06-02 14:17:46 +0600

    Write SetWay method

---
+++ NewFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tdd
+{
+    public class NewFile
+    {
+        public string way;
+        public void SetWay(string p)
+        {
+            way = p;
+        }
+    }
+}
